0c3d147bbac46a2458340b1e391c357a9bf2da87
Nguyễn Thăng Long <thanglongnguyensp@gmail.com>
2017-11-09 18:37:11 +0700

    update all version 2 by longnt

--- code/profile.php
+++ code/profile.php
@@ -17,6 +17,18 @@ if (!isset($_SESSION['name'])) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css"  href="css/css_form.css">
+  <style>
+    .ul1 {
+        background: #808080;
+        padding: 20px;
+    }
+
+   
+    .li1 {
+        background: #FFFAF0;
+        margin: 5px;
+    }
+  </style>
 </head>
 
 <body >
@@ -59,8 +71,8 @@ if (!isset($_SESSION['name'])) {
   
 <!--content-->
 </center>
-  <div class="col-sm-4">
-      <h3>You want to update your information?</h3>
+  <div class="col-sm-6">
+      <center><h2>You want to update your information?</h2></center>
       <form action="profile.php" method="POST">
 
       <div class="form-group">
@@ -109,9 +121,10 @@ if (!isset($_SESSION['name'])) {
       ?>
 </center>
 
-<center>
-  <div class="col-sm-8">
-  <h2>Information about you! </h2>
+
+  <div class="col-sm-6">
+
+  <center><h2>Information about you! </h2></center>
   <br>
         <?php                      
           //ket noi database
@@ -129,15 +142,18 @@ if (!isset($_SESSION['name'])) {
                 $_SESSION['gender'] = $row["gender"];
             }
           ?>
-              <p><span class="glyphicon glyphicon-heart"></span>  User name: <?php echo $_SESSION['uid'];?></p>
-              <p><span class="glyphicon glyphicon-heart"></span>  Full name: <?php echo $_SESSION['fullname']; ?></p>
-              <p><span class="glyphicon glyphicon-heart"></span>  Address: <?php echo $_SESSION['address'];?></p>
-              <p><span class="glyphicon glyphicon-heart"></span>  Phone number: <?php echo $_SESSION['phone'];?></p>
-              <p><span class="glyphicon glyphicon-heart"></span>  Email: <?php echo $_SESSION['email']; ?></p>
-              <p><span class="glyphicon glyphicon-heart"></span>  Gender: <?php echo $_SESSION['gender'] ;?></p>           
+              
+              <ul class="ul1">
+              <li class="li1">User name:    <?php echo $_SESSION['uid'];?></li>
+              <li class="li1">Full name:    <?php echo $_SESSION['fullname']; ?></li>
+              <li class="li1">Address:      <?php echo $_SESSION['address'];?></li>
+              <li class="li1">Phone number: <?php echo $_SESSION['phone'];?></li>
+              <li class="li1">Email:        <?php echo $_SESSION['email']; ?></li>
+              <li class="li1">Gender:       <?php echo $_SESSION['gender'] ;?></li>
+              </ul>
 
   </div>
-</center>
+
 <!--Footer-->
 
 </body>
